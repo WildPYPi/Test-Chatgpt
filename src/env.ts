601851938a38fbf48c61ff4f41ef60402dea9b9d
WildPYPi <119778219+WildPYPi@users.noreply.github.com>
2023-04-22 16:08:31 +0800

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -19,8 +19,8 @@ export const defaultEnv = {
     APIModel: "gpt-3.5-turbo" as Model
   },                                            
   CLIENT_DEFAULT_MESSAGE: `Powered by OpenAI Vercel
-- 本网站仅作为项目演示，不提供服务，请填入自己的 Key，长期使用请 [自行部署](https://github.com/WildPYPi/-Test-Chatgpt-Public)，简单成本低。
-- 点击每条消息前的头像，可以锁定对话，作为角色设定。[查看更多使用技巧](https://github.com/WildPYPi/-Test-Chatgpt-Public#使用技巧)。
+- 本网站仅作为项目演示，不提供服务，请填入自己的 Key，长期使用请 [自行部署](https://github.com/WildPYPi/Test-Chatgpt)，简单成本低。
+- 点击每条消息前的头像，可以锁定对话，作为角色设定。[查看更多使用技巧](https://github.com/WildPYPi/Test-Chatgpt#使用技巧)。
 - 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
 `,
